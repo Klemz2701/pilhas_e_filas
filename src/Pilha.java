3f643718b67135b8dd8b2948133edf6255756b29
Bruno Klemz <107573048+Klemz2701@users.noreply.github.com>
2024-08-14 22:59:48 -0300

    Update Pilha.java

--- src/Pilha.java
+++ src/Pilha.java
@@ -1,12 +1,42 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
-/**
- *
- * @author bruno
- */
 public class Pilha {
-    
+    private int topo;
+    private String[] dados;
+    private int capacidade;
+
+    public Pilha(int capacidade) {
+        this.topo = -1;
+        this.capacidade = capacidade;
+        this.dados = new String[capacidade];
+    }
+
+    public boolean vazia() {
+        return topo == -1;
+    }
+
+    public boolean cheia() {
+        return topo == capacidade - 1;
+    }
+
+    public void insere(String elemento) throws Exception {
+        if (cheia()) {
+            throw new Exception("Pilha cheia");
+        }
+        topo++;
+        dados[topo] = elemento;
+    }
+
+    public String remove() throws Exception {
+        if (vazia()) {
+            throw new Exception("Pilha vazia");
+        }
+        String elementoRemovido = dados[topo];
+        topo--;
+        return elementoRemovido;
+    }
+
+    public void imprime() {
+        for (int i = topo; i >= 0; i--) {
+            System.out.println(dados[i]);
+        }
+    }
 }
